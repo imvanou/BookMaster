cbc46320fb5da8a24b7c4f2e7ed53a1015fc0f16
Vanessa <vanessa.boissierpro@gmail.com>
2025-01-10 10:54:10 +0100

    init story

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route(path: '/login', name: 'auth.login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // gestion erreur
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // email saisi par le user
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('auth/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+       
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
