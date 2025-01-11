6df0a4e659c4e9632d2bbca4919d98eb1045cff4
Vanessa <vanessa.boissierpro@gmail.com>
2025-01-11 11:20:22 +0100

    last push

--- src/Form/BookReadFormType.php
+++ src/Form/BookReadFormType.php
@@ -1,24 +1,57 @@
 <?php
 
+
 namespace App\Form;
 
+use App\Entity\BookRead;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BookReadFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
-        ;
+            ->add('book_id', TextType::class, [
+                'label' => 'Livre',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Mes notes',
+                'required' => false,
+            ])
+            ->add('rating', ChoiceType::class, [
+                'label' => 'Note',
+                'choices' => [
+                    '1' => '1',
+                    '1.5' => '1.5',
+                    '2' => '2',
+                    '2.5' => '2.5',
+                    '3' => '3',
+                    '3.5' => '3.5',
+                    '4' => '4',
+                    '4.5' => '4.5',
+                    '5' => '5',
+                ],
+            ])
+            ->add('is_read', CheckboxType::class, [
+                'label' => 'Lecture terminée',
+                'required' => false,
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer',
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => BookRead::class,
         ]);
     }
 }
